c61918d29f3f7dbb6781cc6316622c84fb80dcd7
ashvinikumar <ashvinikumar45@gmail.com>
2019-06-24 23:01:37 +0530

    loader added

---
+++ src/UI/Spinner/Spinner.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Spinner.css";
+
+const spinner = () => (
+  <div className="text-center spinner">
+    <i className="fa fa-cog fa-spin fa-5x fa-fw" />
+  </div>
+);
+
+export default spinner;
